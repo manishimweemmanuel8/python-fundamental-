03052aa0118c50f7e2748773ab3cbde8a8ce1575
emanishimwe <manishimweemmanuel8@gmail.com>
2023-02-13 08:27:35 +0200

    day three

---
+++ day_three.py
@@ -0,0 +1,23 @@
+# List and dictionary 
+
+my_cart=[12.99,12,20,40]
+sum=sum(my_cart)
+print(sum)
+
+# you can add new thing on my_cart
+my_cart.append(30)
+print(my_cart)
+
+# length of items in the List
+
+length=len(my_cart)
+print(length)
+
+# print one item on position by using indexing this can work on string  like this "Hello"
+print(my_cart[4]+2)
+
+# dictionary
+my_progamming_ranking=[{"programming":"python", "ranking":1 },{"programming":"typescript", "ranking":2, }]
+my_date={"programming":"python", "ranking":1, }
+print(my_date["ranking"])
+print(sort(my_progamming_ranking))
