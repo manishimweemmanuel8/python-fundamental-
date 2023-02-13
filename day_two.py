92a7442cc2b5b726a54ad3ab836a4fbfef6976cc
emanishimwe <manishimweemmanuel8@gmail.com>
2023-02-13 07:59:09 +0200

    Day two

---
+++ day_two.py
@@ -0,0 +1,25 @@
+# Numbers, Strings and variables
+import math
+
+print (math.floor(5** (1/2)))
+
+# this code is not going to multiplier number it is going to multiplier string into 20 times in other word string is set of char
+a="3" * 20
+print(a)
+
+# variables
+
+phrase="this is the phrase"
+number=134
+print (phrase, number)
+
+# quote="this is the wrong "quote" you see"
+# quote="this is the wrong \"quote\" you see" this is best practice
+# quote='this is the wrong "quote" you see' this is working but not best one
+
+# print(quote)
+
+# line break
+
+word="Hello\nword"
+print(word)
